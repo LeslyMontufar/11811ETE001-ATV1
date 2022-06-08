b58bf1a6affe36dd162ac45764042b14f1097ca3
lesly <leslymontufar@ufu.br>
2022-06-08 15:51:50 -0300

    main.c completado

--- src/main.c
+++ src/main.c
@@ -18,13 +18,17 @@
 #define STM32_GPIO_CRL_OFFSET       0x0000  /* Port configuration register low */
 #define STM32_GPIO_CRH_OFFSET       0x0004  /* Port configuration register high */
 
+#define STM32_GPIO_BSRR_OFFSET      0x0010  /* Port bit set/reset register */
+
 
 /* Register Addresses *******************************************************/
 
-#define STM32_RCC_APB2ENR            (STM32_RCC_BASE+STM32_RCC_APB2ENR_OFFSET)
+#define STM32_RCC_APB2ENR               (STM32_RCC_BASE+STM32_RCC_APB2ENR_OFFSET)
 
-#define STM32_GPIO_CRL               (STM32_GPIOC_BASE+STM32_GPIO_CRL_OFFSET)
-#define STM32_GPIO_CRH               (STM32_GPIOC_BASE+STM32_GPIO_CRH_OFFSET)
+#define STM32_GPIOC_CRL                 (STM32_GPIOC_BASE+STM32_GPIO_CRL_OFFSET)
+#define STM32_GPIOC_CRH                 (STM32_GPIOC_BASE+STM32_GPIO_CRH_OFFSET)
+
+#define STM32_GPIOC_BSRR                (STM32_GPIOC_BASE+STM32_GPIO_BSRR_OFFSET)
 
 /* APB2 Peripheral Clock enable register (RCC_APB2ENR) */
 
@@ -47,20 +51,17 @@
 #define GPIO_MODE_OUTPUT_2MHZ         (2) /* Output 2MHz */
 #define GPIO_MODE_OUTPUT_50MHZ        (3) /* Output 50MHz */
 
-/* Máscaras para setar os registradores da memória */
+/* Deslocamento do registrador e Máscaras para setar os registradores da memória */
 
-#define GPIO_MODE_SHIFT(n)            (n < 8) ? (3 << n*4) : (3 << (n - 8)*4)
+#define GPIO_MODE_SHIFT(n)            (n < 8) ? (n << 2) : ((n - 8) << 2)
 #define GPIO_MODE_MASK(n)             (3 << GPIO_MODE_SHIFT(n))
 
-#define GPIO_CNF_SHIFT(n)            (n < 8) ? (3 << n*4 + 2) : (3 << (n - 8)*4 + 2)
-#define GPIO_CNF_MASK(n)             (3 << GPIO_CNF_SHIFT(n))
+#define GPIO_CNF_SHIFT(n)             (n < 8) ? ((n << 2) + 2) : ((n - 8) << 2 + 2)
+#define GPIO_CNF_MASK(n)              (3 << GPIO_CNF_SHIFT(n))
 
-
-// /* GPIO port bit set/reset register */
-// #define GPIO_BSRR_SET(n) (1 << (n))
-// #define GPIO_BSRR_RST(n) (1 << (n + 16))
-
-// OQ EU PRECISO SETAR PARA LIGAR O LED?? E O Q EU PRECISO RESETAR
+/* GPIO port bit set/reset register */
+#define GPIO_BSRR_SET(n)              (1 << (n))
+#define GPIO_BSRR_RST(n)              (1 << (n + 16))
 
 int main(int argc, char *argv[])
 {
@@ -69,9 +70,9 @@ int main(int argc, char *argv[])
 
     /* Ponteiros para registradores */
     uint32_t *pRCC_APB2ENR  = (uint32_t *)STM32_RCC_APB2ENR;
-    uint32_t *pGPIO_CRL  = (uint32_t *)STM32_GPIO_CRL;
-    uint32_t *pGPIO_CRH  = (uint32_t *)STM32_GPIO_CRH;
-
+    uint32_t *pGPIOC_CRL    = (uint32_t *)STM32_GPIOC_CRL;
+    uint32_t *pGPIOC_CRH    = (uint32_t *)STM32_GPIOC_CRH;
+    uint32_t *pGPIOC_BSRR   = (uint32_t *)STM32_GPIOC_BSRR;
 
     /* Habilita clock GPIOC */
     reg  = *pRCC_APB2ENR;
@@ -79,13 +80,16 @@ int main(int argc, char *argv[])
     *pRCC_APB2ENR = reg;
     
     /* Configura PC13 como saida General purpose output push-pull */
-    reg = (PINO_LED >= 8)? *pGPIO_CRH : *pGPIO_CRL;
-    reg &= ~GPIO_MODE_MASK(PINO_LED);
-    reg |= (GPIO_MODE_OUTPUT_50MHZ << GPIO_MODE_SHIFT(PINO_LED));
-    if(PINO_LED >= 8) *pGPIO_CRH = reg; 
-    else *pGPIO_CRL = reg; 
+    reg = (PINO_LED >= 8)? *pGPIOC_CRH : *pGPIOC_CRL;
 
-    // até aqui entendido
+    reg &= ~GPIO_CNF_MASK(PINO_LED);
+    reg |= (GPIO_CNF_O_GPO_PUSH_PULL << GPIO_CNF_SHIFT(PINO_LED));
+
+    reg &= ~GPIO_MODE_MASK(PINO_LED);
+    reg |= (GPIO_MODE_OUTPUT_10MHZ << GPIO_MODE_SHIFT(PINO_LED));
+
+    if(PINO_LED >= 8) *pGPIOC_CRH = reg; 
+    else *pGPIOC_CRL = reg; 
 
     while(1)
     {
